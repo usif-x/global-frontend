1fcef9c9b4aa0a100449a7aa4e56c1b5ee2703d9
usifk <me@usif.tech>
2025-08-31 21:41:07 +0300

    edit next config

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "globaldivers.duckdns.org",
+        hostname: "backend.topdivers.online",
         pathname: "/storage/**",
       },
     ],
