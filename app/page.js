057ef52e93e7c64e120e28069238af77663c14b7
usifk <me@usif.tech>
2026-06-26 16:55:21 +0300

    fix testimontial error

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import DivingCourses from "@/components/section/Course";
 import WhyChooseUs from "@/components/section/Future";
 import Hero from "@/components/section/Hero";
 import PackageTripDisplay from "@/components/section/PackageTrip";
-import Testimonials from "@/components/section/Testimonials";
+import TestimonialShowcase from "@/components/section/Testimonial";
 export const metadata = {
   title: {
     template: "Hurghada Trips - %s",
@@ -55,7 +55,7 @@ export default function Home() {
       <BestSellersHero />
       <DivingCourses />
       <PackageTripDisplay />
-      <Testimonials />
+      <TestimonialShowcase />
     </>
   );
 }
