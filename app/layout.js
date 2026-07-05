d4bb1f3c73ae5a5f7f73fb962c58b9a9acfb7866
usifk <me@usif.tech>
2026-07-05 14:03:23 +0300

    edit layout

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,7 @@ export const metadata = {
     icon: "/favicon.jpg",
   },
   description:
-    "Explore TopDivers with us. Red Sea diving, snorkeling, boat & island tours, safari adventures, and Egypt travel packages for all travelers.",
+    "Explore the Red Sea's Beauty with TopDivers, Hurghada Trips,. Red Sea diving, snorkeling, boat & island tours, safari adventures, and Egypt travel packages for all travelers.",
   keywords:
     "TopDivers, egypt trips, Sunrise, , TopDivers, TopDivers, TopDivers , , TopDivers , safari, marsa alam tours, trips egypt, boat trips hurghada, diving, trips, packages, courses, destinations, safari, egypt, red sea, diving trips, diving packages, diving courses, diving destinations, diving safari, diving egypt, diving red sea, snorkeling hurghada, paradise island hurghada, orange bay trip, giftun island tour, private boat hurghada, speed boat hurghada, luxor day trip from hurghada, cairo trip from hurghada, dolphin house hurghada, hurghada excursions, hurghada activities, hurghada excursions deals, hurghada diving center, scuba diving hurghada, open water course hurghada, padi diving courses egypt, red sea liveaboard, desert safari hurghada, quad safari hurghada, jeep safari hurghada, submarine hurghada, sea scope hurghada, family trips hurghada, couple trips egypt, el gouna trips, sharm el sheikh diving, marsa alam diving, egypt travel packages, red sea holidays, snorkeling egypt, diving holidays egypt, diving safari egypt, daily diving trips hurghada, beginner diving hurghada, advanced diving courses egypt, wreck diving red sea, coral reef diving hurghada, egypt tourism, red sea adventures, hurghada boat tours, luxury trips hurghada, island tours hurghada, diving instructors egypt, learn diving egypt, egypt underwater tours, scuba diving red sea, best diving spots hurghada, eco diving red sea, freediving hurghada, padi certified courses hurghada diving egypt, diving red sea, german-speaking diving center in hurghada, learn to dive with a german-speaking instructor in hurghada, learn diving in hurghada, discover scuba diving hurghada, book diving in hurghada, diving holidays in hurghada, red sea diving trips, top diving center in hurghada, top diving school in hurghada, top diving instructor in hurghada",
   creator: "TopDivers",
@@ -42,7 +42,7 @@ export const metadata = {
   openGraph: {
     title: "TopDivers",
     description:
-      "Explore TopDivers with us. Red Sea diving, snorkeling, boat & island tours, safari adventures, and Egypt travel packages for all travelers.",
+      "Explore the Red Sea's Beauty with TopDivers, Hurghada Trips,. Red Sea diving, snorkeling, boat & island tours, safari adventures, and Egypt travel packages for all travelers.",
     url: "https://topdivers.online",
     siteName: "TopDivers",
     images: [
@@ -61,7 +61,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "TopDivers",
     description:
-      "Explore TopDivers with us. Red Sea diving, snorkeling, boat & island tours, safari adventures, and Egypt travel packages for all travelers.",
+      "Explore the Red Sea's Beauty with TopDivers, Hurghada Trips,. Red Sea diving, snorkeling, boat & island tours, safari adventures, and Egypt travel packages for all travelers.",
     images: ["https://topdivers.online/image/hero-bg.jpg"],
   },
 };
